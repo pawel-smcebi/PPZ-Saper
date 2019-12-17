55c6aacf45fa5b9bee03806ab1bbd1469002de66
Pawel Koarzewski <pawel.kozarzewski@smcebi.edu.pl>
2019-12-17 20:32:53 +0100

    Added in-game timer Added new assets Changed background color Changed window name

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ from gui import MainWidget
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = MainWidget()
+    w.setWindowTitle("Minesweeper")
     w.show()
     sys.exit(app.exec_())
